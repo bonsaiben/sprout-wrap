0ed74b5adc93f5e5e8c7bf66295354a44d603ade
Benjamin Sullivan <benjaminsullivan@Benjamins-MacBook-Pro.local>
2013-08-09 17:20:04 +0900

    my requirements

--- site-cookbooks/meta/recipes/osx_base.rb
+++ site-cookbooks/meta/recipes/osx_base.rb
@@ -1,15 +1,27 @@
-include_recipe "pivotal_workstation::bash_it"
+# install Java runtime
+#include_recipe "pivotal_workstation::bash_it"
 include_recipe "pivotal_workstation::create_var_chef_cache"
 include_recipe "pivotal_workstation::sshd_on"
 include_recipe "pivotal_workstation::screen_sharing_app"
 include_recipe "sprout-osx-apps::skype"
-include_recipe "pivotal_workstation::shiftit"
+#include_recipe "pivotal_workstation::shiftit"
 include_recipe "sprout-osx-apps::firefox"
-include_recipe "pivotal_workstation::inputrc"
+#include_recipe "pivotal_workstation::inputrc"
 include_recipe "pivotal_workstation::locate_on"
 include_recipe "sprout-osx-apps::dropbox"
 include_recipe "sprout-osx-apps::chrome"
-include_recipe "pivotal_workstation::mouse_locator"
+#include_recipe "pivotal_workstation::mouse_locator"
 include_recipe "sprout-osx-settings::defaults_fast_key_repeat_rate"
 include_recipe "pivotal_workstation::menumeters"
-include_recipe "pivotal_workstation::bettertouchtool"
+#include_recipe "pivotal_workstation::bettertouchtool"
+
+# my additions
+include_recipe "pivotal_workstation::sizeup"
+include_recipe "sprout-osx-settings::osx_disable_natural_scrolling"
+include_recipe "sprout-osx-settings::disable_front_row"
+include_recipe "sprout-osx-apps::1password"
+# set caps lock to control
+# google hangouts chrome addon
+# change default chrome search engine to Google English
+# google chrome addon: rikaikun
+# google chrome addon: vimium
--- site-cookbooks/meta/recipes/ruby_development.rb
+++ site-cookbooks/meta/recipes/ruby_development.rb
@@ -1,9 +1,12 @@
-include_recipe "pivotal_workstation::rvm"
+#include_recipe "pivotal_workstation::rvm"
+include_recipe "pivotal_workstation::rbenv"
 include_recipe "pivotal_workstation::gem_setup"
 include_recipe "pivotal_workstation::mysql"
 include_recipe "pivotal_workstation::postgres"
-include_recipe "pivotal_workstation::rubymine"
-include_recipe "pivotal_workstation::rubymine_preferences_pivotal"
 include_recipe "sprout-osx-apps::imagemagick"
-include_recipe "sprout-osx-apps::node_js"
 include_recipe "pivotal_workstation::qt"
+
+# my additions
+include_recipe "sprout-osx-apps::heroku_toolbelt"
+include_recipe "pivotal_workstation::memcached"
+include_recipe "pivotal_workstation::nginx"
